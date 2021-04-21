0560fd70f9963343dc8ed363f185491f3eae7435
kutarobo <kutarobo@naver.com>
2021-04-22 03:33:59 +0900

    컴포넌트 스캔과 의존관계 자동주입 시작하기.

--- src/main/java/study/spring/core/basic/hello/discount/RateDiscountPolicy.java
+++ src/main/java/study/spring/core/basic/hello/discount/RateDiscountPolicy.java
@@ -1,8 +1,10 @@
 package study.spring.core.basic.hello.discount;
 
+import org.springframework.stereotype.Component;
 import study.spring.core.basic.hello.member.Grade;
 import study.spring.core.basic.hello.member.Member;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
 
--- src/main/java/study/spring/core/basic/hello/order/OrderServiceImpl.java
+++ src/main/java/study/spring/core/basic/hello/order/OrderServiceImpl.java
@@ -1,14 +1,18 @@
 package study.spring.core.basic.hello.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import study.spring.core.basic.hello.discount.DiscountPolicy;
 import study.spring.core.basic.hello.member.Member;
 import study.spring.core.basic.hello.member.MemberRepository;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
