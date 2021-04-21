f7f2ff8248f7b3d6a3116e36b8ba41ab75e2f5cd
kutarobo <kutarobo@naver.com>
2021-04-21 17:37:28 +0900

    @Configuration이 싱글톤을 지원하는지 알아보는 테스트코드

---
+++ src/test/java/study/spring/core/basic/hello/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,40 @@
+package study.spring.core.basic.hello.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import study.spring.core.basic.hello.AppConfig;
+import study.spring.core.basic.hello.member.MemberRepository;
+import study.spring.core.basic.hello.member.MemberServiceImpl;
+import study.spring.core.basic.hello.order.OrderServiceImpl;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void ConfigurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig been = ac.getBean(AppConfig.class);
+
+        System.out.println("been.getClass() = " + been.getClass());
+    }
+}
