856f0186727195936acb3e5570e2661c680142fb
kutarobo <kutarobo@naver.com>
2021-05-14 16:35:05 +0900

    feat: Add postConstrict preDestroy
    애노테이션으로 생성 소멸 메서드 만들어서 가져오기

--- src/test/java/study/spring/core/basic/hello/lifecycle/NetworkClient.java
+++ src/test/java/study/spring/core/basic/hello/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package study.spring.core.basic.hello.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 	private String url;
 
@@ -25,6 +28,7 @@ public class NetworkClient {
 		System.out.println("close = " + url);
 	}
 
+	@PostConstruct
 	public void init() {
 		// 의존관계 주입이 끝나면 호출해준다
 		System.out.println("afterPropertiesSet = " + url);
@@ -32,6 +36,7 @@ public class NetworkClient {
 		call("초기화 연결 메세지");
 	}
 
+	@PreDestroy
 	public void close() {
 		System.out.println("close = " + url);
 		disconnect();
